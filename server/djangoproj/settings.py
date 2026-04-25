57e12cb8424a6b2d8ed8c1319fdb9499f99c1060
ev-darner <skye.fox037@gmail.com>
2026-04-25 00:29:08 -0400

    Linting fixes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,8 +29,10 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-'https://evelyndarner-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://evelyndarner-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+        'https://evelyndarner-8000.theiadockernext-1-'
+        'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://evelyndarner-8000.theiadockernext-1'
+                        '-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -95,7 +97,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.'
+        'password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
